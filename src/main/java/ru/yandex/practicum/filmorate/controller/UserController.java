0fd47774f82e6b87aa36109f0d3985fa69cdd0c1
Viktoriia Novikova <koncord@yandex.ru>
2024-09-30 22:08:37 +0300

    Реализовать функциональность "Рекомендации фильмов" на основе предпочтений пользователей с наибольшим числом общих лайков. Удалить классы InMemoryFilmStorage, InMemoryUserStorage и связанные с ними тесты.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
@@ -80,6 +81,11 @@ public class UserController {
     public void deleteFriend(@PathVariable("id") Integer id, @PathVariable("friend-id") Integer friendId) {
         log.info("Пришёл запрос на удаление друга с id: {}", friendId);
         userService.removeFriend(id, friendId);
+    }
+
+    @GetMapping("/{id}/recommendations")
+    List<Film> getRecommendations(@PathVariable Integer id) {
+        return userService.getRecommendations(id);
     }
 
     private void validation(User user) {
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,18 +2,23 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.FriendStorage;
+import ru.yandex.practicum.filmorate.storage.LikeStorage;
 
 import java.util.*;
 
 @Service
 public class UserService {
     private final FriendStorage friendStorage;
+    private final LikeStorage likeStorage;
 
     @Autowired
-    public UserService(FriendStorage friendStorage) {
+    public UserService(FriendStorage friendStorage, LikeStorage likeStorage) {
         this.friendStorage = friendStorage;
+        this.likeStorage = likeStorage;
     }
 
     public void addFriend(Integer userId, Integer friendId) {
@@ -30,5 +35,9 @@ public class UserService {
 
     public List<User> getFriends(int userId) {
         return friendStorage.getFriends(userId);
+    }
+
+    public List<Film> getRecommendations(Integer id) {
+        return likeStorage.getRecommendations(id);
     }
 }
