d97d4290bdf1ea02d15104edee75afb170a0a6bb
JJJ0103 <jjj1.03@yandex.ru>
2024-10-01 23:25:17 +0300

    ААААААААА

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -98,6 +98,10 @@ public class FilmService {
         return likeStorage.getPopularByGenreAndYear(genreId, year, count);
     }
 
+    public List<Film> getFilmsByDirector(Integer directorId, String sortBy) {
+        return filmStorage.getFilmsByDirector(directorId, sortBy);
+    }
+
     public List<Film> getCommonFilms(Integer userId, Integer friendId) {
         if (userId.equals(friendId)) {
             throw new ValidationException("Попытка получить список общих фильмов для одного и того же пользователя");
